dc4ca57f647aec023c97bfc9531283aef7f042ee
Joao Costa <zkozta@gmail.com>
2025-12-08 01:16:07 -0300

    C1 - Inicio do desenvolvimento

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>App de Treino - Configuração Inicial</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
